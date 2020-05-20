dd6ad091f3d0859bff50169f4165ddcd71a5132a
João <contatomesquita@outlook.com>
2020-05-20 08:21:32 -0300

    Criação do component Header.

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card height="100%">
+    <v-app-bar color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>AZI - Licitações e Propostas</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group active-class="primary--text text--accent-4">
+          <v-list-item to="/licitacoes">
+            <v-list-item-icon>
+              <v-icon>mdi-format-list-bulleted-square</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Licitação</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item to="/propostas">
+            <v-list-item-icon>
+              <v-icon>mdi-format-list-bulleted-square</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Proposta</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data: () => ({
+      drawer: false,
+    }),
+  }
+</script>
+
+<style>
+
+</style>
