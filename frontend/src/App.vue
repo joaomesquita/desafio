be40c11b3ef119bf16c07eb3518b837b04e1d579
João <contatomesquita@outlook.com>
2020-05-20 02:51:36 -0300

    Criação do pacote front-end.

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <Header />
+  </v-app>
+</template>
+
+<script>
+  import Header from "./components/Header";
+
+  export default {
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style>
+
+</style>
