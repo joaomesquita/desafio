7976a98dc2612212b75544d448c4d09b5dbae5e8
João <contatomesquita@outlook.com>
2020-06-05 12:53:07 -0300

    Implementação das funções de requisição da api Licitação e Proposta.

--- frontend/src/services/licitacao.js
+++ frontend/src/services/licitacao.js
@@ -1,10 +1,24 @@
-import { api } from './api';
+import { api } from './api'
 
 export default {
-  buscarLicitacoes:() => {
-      return api.get('/licitacao')
+  buscar:() => {
+    return api.get('/licitacao')
   },
-  inserirLicitacao:(licitacao) => {
+
+  buscarPorId:(licitacaoId) => {
+    return api.get(`/licitacao/${licitacaoId}`)
+  },
+
+  inserir:(licitacao) => {
     return api.post('/licitacao', licitacao)
+  },
+
+  atualizar:(licitacaoId, licitacao) => {
+    return api.put(`/licitacao/${licitacaoId}`, licitacao)
+  },
+
+  remover:(licitacaoId) => {
+    return api.delete(`/licitacao/${licitacaoId}`)
   }
 }
+
--- frontend/src/services/proposta.js
+++ frontend/src/services/proposta.js
@@ -1,10 +1,23 @@
 import { api } from './api';
 
 export default {
-  buscarPropostas:() => {
+  buscar:() => {
     return api.get('/proposta')
   },
-  inserirPropostas:(proposta) => {
+
+  buscarPorId:(propostaId) => {
+    return api.get(`/proposta/${propostaId}`)
+  },
+
+  inserir:(proposta) => {
     return api.post('/proposta', proposta)
+  },
+
+  atualizar:(propostaId, proposta) => {
+    return api.put(`/proposta/${propostaId}`, proposta)
+  },
+
+  remover:(propostaId) => {
+    return api.delete(`/proposta/${propostaId}`)
   }
 }
