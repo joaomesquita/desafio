283e72de626e2d71722ca337de40ca97f334cfbc
João <contatomesquita@outlook.com>
2020-06-05 12:56:32 -0300

    Implementação do component AtualizarProposta.

---
+++ frontend/src/pages/proposta/AtualizarProposta.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-flex class="pa-5" xs12>
+    <v-card class="pa-10">
+      <v-card-title>
+        Atualizar Proposta da Licitação #{{ licitacao.licitacaoId }}
+      </v-card-title>
+      <v-card-text>
+        <div class="text-justify">
+          <p v-if="licitacao.classificacaoEnum == 'MENOR_PRECO'">Menor Preço</p>
+          <p v-else>Nota Preço</p>
+          <p>{{ licitacao.descricao }}</p>
+        </div>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field label="Fornecedor" v-model="proposta.fornecedor" required />
+          </v-col>
+          <v-col cols="12" v-if="licitacao.classificacaoEnum == 'NOTA_PRECO'">
+            <v-text-field label="Nota" v-model="proposta.nota" required />
+          </v-col>
+          <v-col cols="12">
+            <v-text-field label="Preço" v-model="proposta.preco" required />
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text @click="$router.push(`/licitacoes/licitacao/${licitacao.licitacaoId}`)">
+          <v-icon left>mdi-arrow-left</v-icon> Voltar
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn class="primary" @click.prevent="atualizarProposta(proposta.propostaId)">
+          <v-icon left>mdi-content-save-outline</v-icon> Salvar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+  import PropostaService from '../../services/proposta'
+  import LicitacaoService from '../../services/licitacao'
+
+  export default {
+    name: "AtualizarProposta",
+    data:() => ({
+      proposta: {
+        propostaId: '',
+        fornecedor: '',
+        nota: '',
+        preco: '',
+      },
+      licitacao: {}
+    }),
+    mounted() {
+      this.buscarLicitacao(this.$route.params.licitacaoId)
+      this.buscarProposta(this.$route.params.propostaId)
+    },
+    methods: {
+      buscarProposta(propostaId) {
+        try {
+          PropostaService.buscarPorId(propostaId)
+            .then(response => {
+              this.proposta = response.data
+              this.proposta.propostaId = propostaId
+              console.log(response.status)
+            })
+        } catch(e) {
+          console.log(e.response.data)
+        }
+      },
+      buscarLicitacao(licitacaoId) {
+        try {
+          LicitacaoService.buscarPorId(licitacaoId)
+            .then(response => {
+              this.licitacao = response.data
+              console.log(response.status)
+            })
+        } catch(e) {
+          console.log(e.response.data)
+        }
+      },
+      atualizarProposta(propostaId) {
+        try {
+          PropostaService.atualizar(propostaId, this.proposta)
+            .then(response => {
+              alert('Atualização realizado com sucesso.')
+              console.log(response.status)
+              this.proposta = {}
+              this.$router.push(`/licitacoes/licitacao/${this.licitacao.licitacaoId}`)
+            })
+        } catch(e) {
+          console.log(e.response.data)
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
